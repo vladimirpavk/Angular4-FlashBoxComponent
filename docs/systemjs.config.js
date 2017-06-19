4f3fcacf24be96287dace9affc8255488714debd
Vladimir Pavković <vladimirpavk@gmail.com>
2017-06-19 11:26:24 +0200

    online demo

--- docs/systemjs.config.js
+++ docs/systemjs.config.js
@@ -6,7 +6,8 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm:': 'node_modules/'
+      'npm_local:': 'node_modules/',
+      'npm': 'https://unpkg.com/'
     },
     // map tells the System loader where to look for things
     map: {
@@ -23,7 +24,7 @@
       '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
       // other libraries
       'rxjs': 'npm:rxjs',         
-      'ng2-flashbox': 'npm:ng2-flashbox'
+      'ng2-flashbox': 'npm_local:ng2-flashbox'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
