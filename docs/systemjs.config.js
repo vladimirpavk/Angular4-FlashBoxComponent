4265363944e7b25cc902c44e479d4a628be7cd65
Vladimir Pavković <vladimirpavk@gmail.com>
2017-06-19 11:49:51 +0200

    +

--- docs/systemjs.config.js
+++ docs/systemjs.config.js
@@ -6,25 +6,22 @@
   System.config({
     paths: {
       // paths serve as alias
-      'npm_local:': 'node_modules/',
-      'npm': 'https://unpkg.com/'
+      //use bottom line for local repositories
+      //'npm:': 'node_modules/'
+      //use bottom line for online repositories
+      'npm:': 'https://unpkg.com/'
     },
     // map tells the System loader where to look for things
     map: {
-      // our app is within the app folder
+      // our app is within the app2 folder
       app: 'app',
       // angular bundles
       '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',    
       '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-      // other libraries
-      'rxjs': 'npm:rxjs',         
-      'ng2-flashbox': 'npm_local:ng2-flashbox'
+      'rxjs': 'npm:rxjs'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -32,14 +29,9 @@
         main: './main.js',
         defaultExtension: 'js'
       },
-      'rxjs': {
+      rxjs: {
         defaultExtension: 'js'
-      },
-      'ng2-flashbox': {
-        //main: './commoncomponents.module.js',
-        main: 'index',
-        defaultExtension: 'js'
-      }
+      }     
     }
   });
 })(this);
