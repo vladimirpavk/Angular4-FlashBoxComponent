b225339b1ff345a9bede22fceb2d15bac732eee2
Vladimir Pavković <vladimirpavk@gmail.com>
2017-06-20 12:56:56 +0200

    Try local for docs

--- docs/systemjs.config.js
+++ docs/systemjs.config.js
@@ -9,7 +9,8 @@
       //use bottom line for local repositories
       //'npm:': 'node_modules/'
       //use bottom line for online repositories
-      'npm:': 'https://unpkg.com/'
+      'npm:': 'https://unpkg.com/',
+      'npm_local': 'node_modules/'      
     },
     // map tells the System loader where to look for things
     map: {
@@ -22,7 +23,8 @@
       '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       'rxjs': 'npm:rxjs',
-      'ng2-flashbox': 'npm:ng2-flashbox@0.2.3/index.js'
+      'ng2-flashbox': 'npm_local:ng2-flashbox/index.js'
+      //'ng2-flashbox': 'npm:ng2-flashbox/index.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
