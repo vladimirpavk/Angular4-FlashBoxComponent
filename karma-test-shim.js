5062e8dbb86fffae29c1f232cbd8026c0b68688f
Vladimir Pavković <vladimirpavk@gmail.com>
2017-07-13 13:49:01 +0200

    Working karma but no spec

--- karma-test-shim.js
+++ karma-test-shim.js
@@ -49,11 +49,20 @@ System.config({
     '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
     '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
     '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
-    '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+    '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js'
+
+    /*'@angular/core/testing': 'base/node_modules/@angular/core/bundles/core-testing.umd.js',
+    '@angular/common/testing': 'base/node_modules/@angular/common/bundles/common-testing.umd.js',
+    '@angular/compiler/testing': 'base/node_modules/@angular/compiler/bundles/compiler-testing.umd.js',
+    '@angular/platform-browser/testing': 'base/node_modules/@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+    '@angular/platform-browser-dynamic/testing': 'base/node_modules/@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+    '@angular/http/testing': 'base/node_modules/@angular/http/bundles/http-testing.umd.js',
+    '@angular/router/testing': 'base/node_modules/@angular/router/bundles/router-testing.umd.js',
+    '@angular/forms/testing': 'base/node_modules/@angular/forms/bundles/forms-testing.umd.js'*/
   },
 });
 
-System.import('base/client/systemjs.config.js') //base is basePath from karma.config.js
+System.import('base/client/systemjs.config.js')
   .then(initTestBed)
   .then(initTesting);
 
--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,5 @@
 module.exports = function(config) {
 
-  var appBase    = 'client/';       // transpiled app JS and map files
-  var appSrcBase = appBase;      // app source TS files
-
-  // Testing helpers (optional) are conventionally in a folder called `testing`
-  var testingBase    = 'testing/'; // transpiled test JS and map files
-  var testingSrcBase = testingBase; // test source TS files
-
   config.set({
     basePath: '',
     frameworks: ['jasmine'],
@@ -16,12 +9,7 @@ module.exports = function(config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter')
     ],
-
-    client: {
-      builtPaths: [appBase, testingBase], // add more spec base paths as needed
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-
+  
     customLaunchers: {
       // From the CLI. Not used here but interesting
       // chrome setup for travis CI using chromium
@@ -60,28 +48,29 @@ module.exports = function(config) {
       { pattern: 'node_modules/@angular/**/*.js', included: false, watched: false },
       { pattern: 'node_modules/@angular/**/*.js.map', included: false, watched: false },
 
-      { pattern: appBase + '/systemjs.config.js', included: false, watched: true },      
+      { pattern: 'client/systemjs.config.js', included: false, watched: true },      
       'karma-test-shim.js', // optionally extend SystemJS mapping e.g., with barrels
 
       // transpiled application & spec code paths loaded via module imports
-      { pattern: appBase + '**/*.js', included: false, watched: true },      
-      { pattern: appBase + '**/*.spec.js', included: false, watched: true },
+      { pattern: 'client/ap/modules/commoncomponents/components/flashbox/1.spec.*', included: false, watched: true},
+      { pattern: 'cliet/**/*.spec.js', included: false, watched: true},
+      { pattern: 'client/**/*.js', included: false, watched: true },            
 
       // Asset (HTML & CSS) paths loaded via Angular's component compiler
       // (these paths need to be rewritten, see proxies section)
-      { pattern: appBase + '**/*.html', included: false, watched: true },
-      { pattern: appBase + '**/*.css', included: false, watched: true },
+      { pattern: 'client/**/*.html', included: false, watched: true },
+      { pattern: 'client/**/*.css', included: false, watched: true },
 
       // Paths for debugging with source maps in dev tools
-      { pattern: appBase + '**/*.ts', included: false, watched: false }//,
-      //{ pattern: appBase + '**/*.js.map', included: false, watched: false }      
+      { pattern: 'client/**/*.ts', included: false, watched: true },
+      { pattern: 'client/**/*.js.map', included: false, watched: true }      
     ],
 
     // Proxied base paths for loading assets
-    /*proxies: {
+    proxies: {
       // required for modules fetched by SystemJS
-      '/base/src/node_modules/': './node_modules'
-    },*/
+      '/node_module/': '/base/node_modules/'
+    },
 
     exclude: [],
     preprocessors: {},
