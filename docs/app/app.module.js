a06172ca18748092eca7de96814dcac01e103f6e
Vladimir Pavković <vladimirpavk@gmail.com>
2017-09-29 14:20:09 +0200

    Retrieved backup

--- docs/app/app.module.js
+++ docs/app/app.module.js
@@ -1,28 +1,43 @@
-"use strict";
-var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
-    return c > 3 && r && Object.defineProperty(target, key, r), r;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-/// <reference path="../../node_modules/@angular/common/common.d.ts" />
-const core_1 = require("@angular/core");
-const platform_browser_1 = require("@angular/platform-browser");
-const app_component_1 = require("./app.component");
-const commoncomponents_module_1 = require("./modules/commoncomponents/commoncomponents.module");
-let AppModule = class AppModule {
-};
-AppModule = __decorate([
-    core_1.NgModule({
-        imports: [
-            commoncomponents_module_1.CommonComponentsModule,
-            platform_browser_1.BrowserModule
+System.register(["@angular/core", "@angular/platform-browser", "./app.component", "ng2-flashbox"], function (exports_1, context_1) {
+    "use strict";
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __moduleName = context_1 && context_1.id;
+    var core_1, platform_browser_1, app_component_1, ng2_flashbox_1, AppModule;
+    return {
+        setters: [
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (platform_browser_1_1) {
+                platform_browser_1 = platform_browser_1_1;
+            },
+            function (app_component_1_1) {
+                app_component_1 = app_component_1_1;
+            },
+            function (ng2_flashbox_1_1) {
+                ng2_flashbox_1 = ng2_flashbox_1_1;
+            }
         ],
-        declarations: [app_component_1.AppComponent],
-        bootstrap: [app_component_1.AppComponent]
-    })
-], AppModule);
-exports.AppModule = AppModule;
-
+        execute: function () {
+            AppModule = class AppModule {
+            };
+            AppModule = __decorate([
+                core_1.NgModule({
+                    imports: [
+                        ng2_flashbox_1.CommonComponentsModule,
+                        platform_browser_1.BrowserModule
+                    ],
+                    declarations: [app_component_1.AppComponent],
+                    bootstrap: [app_component_1.AppComponent]
+                })
+            ], AppModule);
+            exports_1("AppModule", AppModule);
+        }
+    };
+});
 //# sourceMappingURL=app.module.js.map
--- docs/app/app.module.ts
+++ docs/app/app.module.ts
@@ -2,7 +2,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
-import { CommonComponentsModule } from './modules/commoncomponents/commoncomponents.module';
+import { CommonComponentsModule } from 'ng2-flashbox';
 
 @NgModule({
   imports:      [ 
