bbe1376fd83e285d7492feae43d51df0fcba89a8
Vladimir Pavković <vladimirpavk@gmail.com>
2017-10-03 13:50:44 +0200

    Changed docs

--- docs/systemjs.config.js
+++ docs/systemjs.config.js
@@ -2,39 +2,31 @@
  * System configuration for Angular samples
  * Adjust as necessary for your application needs.
  */
-(function (global) {
-  System.config({
-    paths: {
-      // paths serve as alias
-      //use bottom line for local repositories
-      //'npm:': 'node_modules/'
-      //use bottom line for online repositories
-      'npm:': 'https://unpkg.com/',
-      'npm_local': 'docs/node_modules/'      
-    },
-    // map tells the System loader where to look for things
-    map: {
-      // our app is within the app2 folder
-      app: 'app',
-      // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',    
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      'rxjs': 'npm:rxjs',
-      //'ng2-flashbox': 'npm_local:ng2-flashbox/index.js'
-      'ng2-flashbox': 'npm:ng2-flashbox/index.js'
-    },
-    // packages tells the System loader how to load when no filename and/or no extension
-    packages: {
-      app: {
-        main: './main.js',
-        defaultExtension: 'js'
-      },
-      rxjs: {
-        defaultExtension: 'js'
-      }     
-    }
-  });
+(function(global) {
+    System.config({
+        paths: {
+            'npm:': 'https://unpkg.com/',
+        },
+        map: {
+            app: 'app',
+            // angular bundles
+            '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+            '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            'rxjs': 'npm:rxjs',
+            'ng2-flashbox': 'npm:ng2-flashbox/index.js'
+        },
+        // packages tells the System loader how to load when no filename and/or no extension
+        packages: {
+            app: {
+                main: './main.js',
+                defaultExtension: 'js'
+            },
+            rxjs: {
+                defaultExtension: 'js'
+            }
+        }
+    });
 })(this);
